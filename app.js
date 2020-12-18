78fda6d78a54383687970bfc280d732cdbc17450
Laurie Cai <laurie801@gmail.com>
2020-12-18 13:00:05 -0800

    Add 'Copy Link' and 'Next GIF' button functionality

--- app.js
+++ app.js
@@ -5,5 +5,24 @@ fetch('https://api.giphy.com/v1/gifs/translate?api_key=k4rXUclbltEhgYXZHyKuxVa39
         return response.json();
     })
     .then(function(response) {
-        img.src = (response.data.images.original.url);
+        img.src = response.data.images.original.url;
+        
+        const nextGif = document.getElementById('next-gif-btn');
+        nextGif.addEventListener('click', () => {
+            window.location.reload();
+        })
+
+        const tempInput = document.createElement('textArea');
+        const main = document.querySelector('.main');
+        main.appendChild(tempInput);
+        tempInput.value = img.src;
+
+        const copyLink = document.getElementById('copy-link-btn');
+        copyLink.addEventListener('click', () => {
+            // select text
+            tempInput.select();
+            // copy it
+            document.execCommand('copy');
+            console.log('link copied!');
+        })
     });
